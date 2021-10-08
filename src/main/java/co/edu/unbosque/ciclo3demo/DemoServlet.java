304a957064fc846a5bf42d1b34f266355054b758
Primer Commit <william.enciso@cun.edu.co>
2021-10-08 07:17:56 -0500

    Update y Delete agregados

--- src/main/java/co/edu/unbosque/ciclo3demo/DemoServlet.java
+++ src/main/java/co/edu/unbosque/ciclo3demo/DemoServlet.java
@@ -1,7 +1,5 @@
 package co.edu.unbosque.ciclo3demo;
 
-
-
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
@@ -19,70 +17,128 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/DemoServlet")
 public class DemoServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public DemoServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public DemoServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
-		
-		  String listar = request.getParameter("Listar"); 
-		  String agregar = request.getParameter("Agregar");
-		  String ingresar = request.getParameter("Ingresar");
-		  String actualizar = request.getParameter("Actualizar");
-		  String borrar = request.getParameter("Borrar");
-		  
-		  if(agregar != null) { agregarUsuario(request, response); }
-		  
-		  if(listar != null) { listarUsuarios(request, response); }
-		  
-		  if(ingresar != null) { validarCredenciales(request, response); }
-		 
+
+		String listar = request.getParameter("Listar");
+		String agregar = request.getParameter("Agregar");
+		String ingresar = request.getParameter("Ingresar");
+		String actualizar = request.getParameter("Actualizar");
+		String borrar = request.getParameter("Borrar");
+
+		if (agregar != null) {
+			agregarUsuario(request, response, agregar);
+		}
+
+		if (listar != null) {
+			listarUsuarios(request, response);
+		}
+
+		if (ingresar != null) {
+			validarCredenciales(request, response);
+		}
+
+		if (actualizar != null) {
+			agregarUsuario(request, response, actualizar);
+		}
+
+		if (borrar != null) {
+			borrarUsuarios(request, response);
+		}
+
 	}
-	
+
 	public void validarCredenciales(HttpServletRequest request, HttpServletResponse response) throws IOException {
 		String usuarioValidate = request.getParameter("usuario");
 		String passValidate = request.getParameter("password");
 		PrintWriter writer = response.getWriter();
-		
-		if (usuarioValidate.equalsIgnoreCase("admininicial") && passValidate.equalsIgnoreCase("admin123456") ) {
+
+		if (usuarioValidate.equalsIgnoreCase("admininicial") && passValidate.equalsIgnoreCase("admin123456")) {
 			writer.println("Usuario Correcto!");
-		}else {
+		} else {
 			writer.println("Usuario Erroneo!");
 		}
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
-	
-	public void agregarUsuario(HttpServletRequest request, HttpServletResponse response) {
-		Usuarios usuario = new Usuarios();
-		usuario.setNombre_usuario(request.getParameter("nombre"));
-		usuario.setCedula_usuario(request.getParameter("cedula"));
-		usuario.setEmail_usuario(request.getParameter("email"));
-		usuario.setUsuario(request.getParameter("usuario"));
-		usuario.setPassword(request.getParameter("password"));
+
+	public void agregarUsuario(HttpServletRequest request, HttpServletResponse response, String accion) {
 		int respuesta = 0;
+		String pagina = "/usuarios.jsp";
+		Usuarios usuario;
+		
+		if (accion.compareToIgnoreCase("Actualizar") == 0) {
+			String nombre = request.getParameter("nombre");
+			String cedula = request.getParameter("cedula");
+			String email = request.getParameter("email");
+			String user = request.getParameter("usuario");
+			String password = request.getParameter("password");
+			
+			usuario = new Usuarios();
+			if ((nombre != null) && (nombre.length() > 0)) {
+				System.out.println(nombre);
+				usuario.setNombre_usuario(nombre);
+			}
+			
+			if (cedula.length() > 0) {
+				System.out.println(cedula);
+				usuario.setCedula_usuario(cedula);
+			}
+			
+			if ((email != null) && (email.length() > 0)) {
+				System.out.println(email);
+				usuario.setEmail_usuario(email);
+			}
+			
+			if ((user != null) && (user.length() > 0)) {
+				usuario.setUsuario(user);
+			}
+			
+			if ((password != null) && (password.length() > 0)) {
+				usuario.setPassword(password);
+			}
+			
+			
+		} else {
+			usuario = new Usuarios();
+			usuario.setNombre_usuario(request.getParameter("nombre"));
+			usuario.setCedula_usuario(request.getParameter("cedula"));
+			usuario.setEmail_usuario(request.getParameter("email"));
+			usuario.setUsuario(request.getParameter("usuario"));
+			usuario.setPassword(request.getParameter("password"));
+		}
+		
+
 		try {
-			respuesta = TestJSON.postJSON(usuario);
+			respuesta = TestJSON.postJSON(usuario, accion);
 			PrintWriter writer = response.getWriter();
 			if (respuesta == 200) {
-				writer.println("Registro Agregado!");
-			}else {
+				RequestDispatcher dispatcher = getServletContext().getRequestDispatcher(pagina);
+				dispatcher.forward(request, response);
+			} else {
 				writer.println("Error " + respuesta);
 			}
 			writer.close();
@@ -91,7 +147,7 @@ public class DemoServlet extends HttpServlet {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public void listarUsuarios(HttpServletRequest request, HttpServletResponse response) {
 		// TODO Auto-generated method stub
 		try {
@@ -100,6 +156,43 @@ public class DemoServlet extends HttpServlet {
 			request.setAttribute("lista", lista);
 			RequestDispatcher dispatcher = getServletContext().getRequestDispatcher(pagina);
 			dispatcher.forward(request, response);
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+	}
+
+	/*
+	 * public void actualizarUsuarios(HttpServletRequest request,
+	 * HttpServletResponse response) { Usuarios usuario = new Usuarios();
+	 * usuario.setNombre_usuario(request.getParameter("nombre"));
+	 * usuario.setCedula_usuario(request.getParameter("cedula"));
+	 * usuario.setEmail_usuario(request.getParameter("email"));
+	 * usuario.setUsuario(request.getParameter("usuario"));
+	 * usuario.setPassword(request.getParameter("password")); int respuesta = 0; try
+	 * { respuesta = TestJSON.postJSON(usuario); 
+	 * PrintWriter writer = response.getWriter();
+	 *  if (respuesta == 200) {
+	 * writer.println("Registro Agregado!"); }else { writer.println("Error " +
+	 * respuesta); } writer.close(); } catch (Exception e) { // TODO: handle
+	 * exception e.printStackTrace(); } }
+	 */
+
+	public void borrarUsuarios(HttpServletRequest request, HttpServletResponse response) {
+		int respuesta = 0;
+		Usuarios usuario;
+		try {
+			usuario = new Usuarios();
+			usuario.setCedula_usuario(request.getParameter("cedula"));
+			respuesta = TestJSON.deleteJSON(usuario);
+			PrintWriter writer = response.getWriter();
+			if (respuesta == 200) {
+				writer.println("Registro Eliminado!");
+			} else {
+				writer.println("Error " + respuesta);
+			}
+			writer.close();
+			
 		} catch (Exception e) {
 			// TODO: handle exception
 			e.printStackTrace();
--- src/main/java/co/edu/unbosque/ciclo3demo/TestJSON.java
+++ src/main/java/co/edu/unbosque/ciclo3demo/TestJSON.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
@@ -60,16 +61,33 @@ public class TestJSON {
 		return lista;
 	}
 	
-	public static int postJSON(Usuarios usuario) throws IOException {
-		url = new URL(sitio + "usuarios/guardar");
-		HttpURLConnection http;
-		http = (HttpURLConnection)url.openConnection();
-		String authStr = Base64.getEncoder().encodeToString("usuario:tiendagenerica".getBytes());
-		try {
-		http.setRequestMethod("POST");
-		} catch (ProtocolException e) {
-		e.printStackTrace();
+	public static int postJSON(Usuarios usuario, String accion) throws IOException {
+		
+		HttpURLConnection http = null;
+		String authStr = "";
+		
+		if (accion.compareToIgnoreCase("Agregar") == 0) {
+			url = new URL(sitio + "usuarios/guardar");
+			http = (HttpURLConnection)url.openConnection();
+			authStr = Base64.getEncoder().encodeToString("usuario:tiendagenerica".getBytes());
+			try {
+			http.setRequestMethod("POST");
+			} catch (ProtocolException e) {
+			e.printStackTrace();
+			}
 		}
+		
+		if (accion.compareToIgnoreCase("Actualizar") == 0) {
+			url = new URL(sitio + "usuarios/actualizar");
+			http = (HttpURLConnection)url.openConnection();
+			authStr = Base64.getEncoder().encodeToString("usuario:tiendagenerica".getBytes());
+			try {
+			http.setRequestMethod("PUT");
+			} catch (ProtocolException e) {
+			e.printStackTrace();
+			}
+		}
+		
 		http.setDoOutput(true);
 		http.setRequestProperty("Accept", "application/json");
 		http.setRequestProperty("Authorization", "Basic " + authStr);
@@ -84,6 +102,28 @@ public class TestJSON {
 		byte[] out = data.getBytes(StandardCharsets.UTF_8);
 		OutputStream stream = http.getOutputStream();
 		stream.write(out);
+		int respuesta = http.getResponseCode();
+		http.disconnect();
+		return respuesta;
+	}
+
+	public static int deleteJSON(Usuarios usuario) throws IOException {
+		// TODO Auto-generated method stub
+		HttpURLConnection http = null;
+		String authStr = "";
+		
+		url = new URL(sitio + "usuarios/eliminar/" + usuario.getCedula_usuario());
+		http = (HttpURLConnection)url.openConnection();
+		authStr = Base64.getEncoder().encodeToString("usuario:tiendagenerica".getBytes());
+		try {
+			http.setRequestMethod("DELETE");
+		} catch (ProtocolException e) {
+		e.printStackTrace();
+		}
+		http.setDoOutput(true);
+		http.setRequestProperty("Accept", "application/json");
+		http.setRequestProperty("Authorization", "Basic " + authStr);
+		http.setRequestProperty("Content-Type", "application/json");
 		int respuesta = http.getResponseCode();
 		http.disconnect();
 		return respuesta;
